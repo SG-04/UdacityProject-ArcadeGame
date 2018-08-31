5dc9f9779f514df12088db9d10aa6184a4febf5e
Shruti Gupta <shrutigupta.sg33@gmail.com>
2018-08-31 16:39:48 +0530

    Implementing Player class

--- js/app.js
+++ js/app.js
@@ -41,7 +41,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function () {
+    this.sprite = 'images/char-pink-girl.png';
+    this.x = 200;
+    this.y = 320;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
